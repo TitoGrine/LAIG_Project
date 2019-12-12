5e3f42872c39022e9887bc72e1fd951aa5292fa5
ManelCoutinho <manuel.monge.coutinho@gmail.com>
2019-12-12 12:37:46 +0000

    Plog API

--- Project 3/src/api.pl
+++ Project 3/src/api.pl
@@ -19,17 +19,41 @@ initialize_board(Rows, Columns, FinalBoard):-
 % Is true when given points are the maximum calculatable in the Board.
 % Uses @see value to calculate the biggest isle of a certain disc color
 % @param Board must be a list of lists
+% Player: 0 or 1
 pontuation(Board, Player, Points) :-
 	select_piece(Player, Disc),
 	value(Visited, Disc, MaxPoints).
 
 %! hasMoves(+Board, +Player, -Ret)
 % Determines if the Player has any moves left
+% Player: 0 or 1
 % Return true by the Ret parameter if there is any Moves left, false otherwise
 hasMoves(Board, Player, Ret) :-
 	(valid_moves(Board, Player, []),
 	Ret = false) ;
 	Ret = true.
 
+%! getPlayerMoves(+Board, +Player, -Moves)
+% Gets all the valid player moves 
+% Uses @see valid_moves
+% Player: 0 or 1
+getPlayerMoves(Board, Player, Moves) :-
+	valid_moves(Board, Player, Moves).
 
+%! playerMove(+Board, +Player, +Move, -NewBoard)
+% Gets Board after player move
+% Uses @see move
+% Move format: [IC, IR, FC, FR]
+% Player: 0 or 1
+playerMove(Board, Player, Move, NewBoard) :-
+	move(Board, Move, NewBoard, Player).
+
+%! botMove(+Board, +Player, +Difficulty, -NewBoard)
+% Gets Board after bot move
+% Uses @see choose_move (to choose move automatically) and @see move
+% Player: 0 or 1
+% Difficulty: 0 .. 4
+botMove(Board, Player, Difficulty, NewBoard) :-
+	choose_move(Board, [Player, bot], NewMove, Difficulty),
+	move(Board, NewMove, NewBoard, Player).
 
